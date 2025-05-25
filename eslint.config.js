939c888356d8115d1a045cbc965567d20b78fa37
Muhammad Naufal Farras <opal.farras@gmail.com>
2025-05-25 08:23:42 +0700

    installing cypress

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import cypress from 'eslint-plugin-cypress/flat';
 import daStyle from 'eslint-config-dicodingacademy';
 
 export default [
@@ -21,6 +22,7 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      cypress: cypress,
     },
     rules: {
       ...js.configs.recommended.rules,
